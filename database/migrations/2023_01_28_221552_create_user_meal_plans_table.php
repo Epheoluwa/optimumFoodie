957e7989b46cdc103ed4f12c11e15fa266e4913d
Solomon Sunmola <sunmolasolomon@gmail.com>
2023-01-28 23:43:39 +0100

    creating mode and migration for usermeal plan

--- database/migrations/2023_01_28_221552_create_user_meal_plans_table.php
+++ database/migrations/2023_01_28_221552_create_user_meal_plans_table.php
@@ -15,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('user_meal_plans', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('days');
+            $table->string('month_par');
+            $table->string('sex');
+            $table->integer('age');
+            $table->integer('height');
+            $table->integer('weight');
+            $table->json('goal');
+            $table->string('weight_aim')->nullable();
+            $table->integer('weight_time_aim')->nullable();
+            $table->json('activity');
+            $table->integer('calories');
+            $table->integer('main_meal');
+            $table->integer('snack_meal');
+            $table->json('food_options');
+            $table->json('daymeal');
             $table->timestamps();
         });
     }
